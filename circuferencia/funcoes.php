e472ef805a5316dca91bde16d8bf04893d4c3e7d
Alê <110263831+ale09carvalho@users.noreply.github.com>
2025-03-10 21:48:09 -0300

    Create funcoes.php
    funcoes calcular area e tamanho do circulo

---
+++ circuferencia/funcoes.php
@@ -0,0 +1,20 @@
+<?php
+/* 
+* Crie um programa onde o usuario informa o tamanho do * raio de um circulo, e o programa
+* retorna a area do circulo e o tamanho da circuferência 
+* obs: use a funçao pi() do PHP para pegar o verdadeiro valor do PI.
+area = pi * raio2
+*/
+
+// funçao para calcular a area do circulo
+function calcularAreaCirculo($r){
+    return pi()*$r**2;
+}
+   
+//funcoa tamnho da circuferencia  
+function calcularCircuferência ($r){
+    {
+     return 2*pi()*$r;
+}
+
+?>
