da822c67e4b47e3935f58aba1e690c1b8da272c1
Alê <110263831+ale09carvalho@users.noreply.github.com>
2025-03-22 16:17:54 -0300

    Create result.php
    receber dados do formulario

---
+++ 1aula/result.php
@@ -0,0 +1,10 @@
+<?php
+    //declaraçao de variavel
+    $nome = $_POST['nome'];
+    //saida de dados
+    echo "ola, seja bem vindo! " .$nome. ".";
+    
+    //link par voltar para pagina anterior
+    echo '<br/><a href= "index.html"> Voltar</a>';
+    
+?>
