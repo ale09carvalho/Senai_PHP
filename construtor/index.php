a1a715b3b5af04815b523fa0560d695ce71ae84e
Alê <110263831+ale09carvalho@users.noreply.github.com>
2025-03-21 21:30:53 -0300

    Create index.php
    pagina  principal

---
+++ construtor/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Prog Orientada Objeto - CONSTRUTOR</title>
+</head>
+<body>
+    <form action="result.php" method="post">
+        <label for="nome">Nome:</label>
+        <input type="text" name="nome" id="nome" required>
+        <br><br>
+        <label for="idade">Idade:</label>
+        <input type="number" name="idade" id="idade" required> 
+        <br><br>
+        <label for="email">email:</label>
+        <input type="email" name="email" id="email" required>
+        <br><br>
+        <label for="cargo" >Cargo:</label>
+        <input type="text" name="cargo" required>
+        <br><br>
+        <button type="submit">Enviar</button>
+    </form>
+</body>
+</html>
